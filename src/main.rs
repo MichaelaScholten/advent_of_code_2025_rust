a4fc5272b22d22e26cb79fce441bfc4d266b5710
Michael Scholten <michael-scholten@hotmail.nl>
2025-12-01 21:12:18 +0100

    Decreased the number of allocations

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,40 @@ use std::{
 
 fn calculate_dial0(path: impl AsRef<Path>) -> u16 {
     let mut number = 50;
-    BufReader::new(File::open(path).unwrap())
-        .lines()
-        .map(Result::unwrap)
-        .filter(|line| !line.trim().is_empty())
-        .map(|line| {
-            let direction = line.chars().next().unwrap();
-            let mut rotation = line[1..].parse::<u16>().unwrap();
-            let mut zeros = rotation / 100;
-            rotation %= 100;
-            number = if direction == 'L' {
-                if number < rotation {
-                    if number != 0 {
-                        zeros += 1;
-                    }
-                    100 - rotation + number
-                } else {
-                    number - rotation
-                }
-            } else {
-                if number + rotation > 100 {
+    let mut lines = BufReader::new(File::open(path).unwrap());
+    let mut line = String::new();
+    let mut zeros = 0;
+    while let Ok(bytes_read) = lines.read_line(&mut line) {
+        if bytes_read == 0 {
+            break;
+        }
+
+        let direction = line.chars().next().unwrap();
+        let mut rotation = line[1..].trim().parse::<u16>().unwrap();
+        zeros += rotation / 100;
+        rotation %= 100;
+        number = if direction == 'L' {
+            if number < rotation {
+                if number != 0 {
                     zeros += 1;
                 }
-                (number + rotation) % 100
-            };
-            if number == 0 {
+                100 - rotation + number
+            } else {
+                number - rotation
+            }
+        } else {
+            if number + rotation > 100 {
                 zeros += 1;
             }
-            zeros
-        })
-        .sum()
+            (number + rotation) % 100
+        };
+        if number == 0 {
+            zeros += 1;
+        }
+
+        line.clear();
+    }
+    zeros
 }
 
 fn main() {

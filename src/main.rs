8580e1418bab17bb8a6c981ddeb5c07dcfd8fe18
Michael Scholten <michael-scholten@hotmail.nl>
2025-12-03 19:02:36 +0100

    Day 2, exercise 1

--- src/main.rs
+++ src/main.rs
@@ -4,70 +4,49 @@ use std::{
     path::Path,
 };
 
-/// Counts the number of times the dial hits 0
-fn calculate_dial0(path: impl AsRef<Path>) -> u16 {
-    // Initialize the current dial position (number), file buffer, line buffer, and zero count
-    let mut number = 50;
-    let mut file = BufReader::new(File::open(path).unwrap());
-    let mut line = String::new();
-    let mut zeros = 0;
+fn invalid_id(id: &str) -> bool {
+    id.starts_with('0') || id[..id.len() / 2] == id[id.len() / 2..]
+}
 
-    // Iterate through the lines
-    while let Ok(bytes_read) = file.read_line(&mut line) {
-        // Stop reading if nothing was read
-        if bytes_read == 0 {
-            break;
-        }
+// Too high
+fn sum_invalid_ids(path: impl AsRef<Path>) -> u64 {
+    BufReader::new(File::open(path).unwrap())
+        .split(b',')
+        .map(Result::unwrap)
+        .map(|range| {
+            let mut parts = range.split(|&byte| byte == b'-');
+            let first = str::from_utf8(parts.next().unwrap()).unwrap();
+            let second = str::from_utf8(parts.next().unwrap()).unwrap();
+            assert!(parts.next().is_none());
 
-        // Take the first char to determine the rotation direction
-        let direction = line.as_bytes()[0];
-
-        // Read the other bytes of the line to determine how much to rotate by
-        let mut rotation = line[1..].trim().parse::<u16>().unwrap();
-
-        // Increase the number of zeros by the number of full rotations and limit the amount to
-        // rotate by to an amount below 100.
-        zeros += rotation / 100;
-        rotation %= 100;
-
-        // Rotate and increment the number of zeros if needed
-        number = if direction == b'L' {
-            if number < rotation {
-                if number != 0 {
-                    zeros += 1;
-                }
-                100 - rotation + number
-            } else {
-                number - rotation
+            let first_int = first.trim().parse::<u64>().unwrap();
+            let second_int = second.trim().parse().unwrap();
+            let mut bad_sum = 0;
+            if invalid_id(first) {
+                bad_sum += first_int;
             }
-        } else {
-            if number + rotation > 100 {
-                zeros += 1;
+            if invalid_id(second) {
+                bad_sum += second_int;
             }
-            (number + rotation) % 100
-        };
-        if number == 0 {
-            zeros += 1;
-        }
-
-        // Clear the buffer
-        line.clear();
-    }
-
-    zeros
+            bad_sum
+                + (first_int..second_int)
+                    .skip(1)
+                    .filter(|&number| invalid_id(&number.to_string()))
+                    .sum::<u64>()
+        })
+        .sum()
 }
 
 fn main() {
-    println!("{}", calculate_dial0("data.txt"));
+    println!("{}", sum_invalid_ids("data.txt"));
 }
 
 #[cfg(test)]
 mod tests {
-
-    use crate::calculate_dial0;
+    use crate::sum_invalid_ids;
 
     #[test]
-    fn test1() {
-        assert_eq!(calculate_dial0("test.txt"), 6);
+    fn test() {
+        assert_eq!(sum_invalid_ids("test.txt"), 1227775554);
     }
 }

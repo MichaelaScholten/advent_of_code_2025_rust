48db900b6afc365915a55f30fda720d6a5b20384
Michael Scholten <michael-scholten@hotmail.nl>
2025-12-03 19:20:25 +0100

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::{
 };
 
 fn invalid_id(id: &str) -> bool {
+    // If the id starts with 0 or the digits repeat each other (individually or as group)
+    // it's invalid.
     id.starts_with('0')
         || (2..=id.len()).any(|splits| {
             if !id.len().is_multiple_of(splits) {
@@ -15,19 +17,28 @@ fn invalid_id(id: &str) -> bool {
         })
 }
 
-// Too high
 fn sum_invalid_ids(path: impl AsRef<Path>) -> u64 {
+    // Read the data file
     BufReader::new(File::open(path).unwrap())
+        // Split on ranges and panic on errrors
         .split(b',')
         .map(Result::unwrap)
         .map(|range| {
+            // Split the range in 2
             let mut parts = range.split(|&byte| byte == b'-');
+
+            // Turn both parts into strings
             let first = str::from_utf8(parts.next().unwrap()).unwrap();
             let second = str::from_utf8(parts.next().unwrap()).unwrap();
+
+            // Panic if the range contains more than 2 values
             assert!(parts.next().is_none());
 
+            // Parse both parts of the range as integers
             let first_int = first.trim().parse::<u64>().unwrap();
             let second_int = second.trim().parse().unwrap();
+
+            // Check for invalid id's in the range and sum them
             let mut bad_sum = 0;
             if invalid_id(first) {
                 bad_sum += first_int;
